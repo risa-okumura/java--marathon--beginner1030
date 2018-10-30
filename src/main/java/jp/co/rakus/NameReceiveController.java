f97ad5612d2004e2814b34b3683be79c55c802ff
okumura <risa.okumura@rakus.co.jp>
2018-10-30 14:31:41 +0900

    1kmマラソンに関すクラス、jspを作成

--- src/main/java/jp/co/rakus/NameReceiveController.java
+++ src/main/java/jp/co/rakus/NameReceiveController.java
@@ -1,7 +1,10 @@
 package jp.co.rakus;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/nameReceive")
 public class NameReceiveController {
 	
@@ -10,7 +13,9 @@ public class NameReceiveController {
 		return "inputname";
 	}
 	
-	public String input(String name) {
+	@RequestMapping("/input")
+	public String input(String name,Model model) {
+		model.addAttribute("name",name);
 		return "outputname";
 	}
 	
--- src/main/webapp/WEB-INF/views/outputname.jsp
+++ src/main/webapp/WEB-INF/views/outputname.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,6 +7,8 @@
 <title>Insert title here</title>
 </head>
 <body>
+ようこそ<br>
+<c:out value="${name}"/>さん
 
 </body>
 </html>
